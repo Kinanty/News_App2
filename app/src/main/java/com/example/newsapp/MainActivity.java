416de064feadcfcc8b05d60f69ed8fa13a8a84ff
Kinanty <kinantykusuma@gmail.com>
2020-08-18 11:27:53 +0700

    add SP

--- app/src/main/java/com/example/newsapp/MainActivity.java
+++ app/src/main/java/com/example/newsapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.newsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,10 @@ import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    SharedPreferences pref;
+    SharedPreferences.Editor editor;
+
     EditText edt_username;
     EditText edt_password;
     Button btn_forgetpass;
@@ -21,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pref = getSharedPreferences("login", MODE_PRIVATE);
         edt_username = findViewById(R.id.edt_username);
         edt_password = findViewById(R.id.edt_password);
         btn_forgetpass = findViewById(R.id.btn_forgetpass);
@@ -32,9 +38,13 @@ public class MainActivity extends AppCompatActivity {
                 if(edt_username.getText().toString().equalsIgnoreCase("Fino")&&
                         edt_password.getText().toString().equalsIgnoreCase("alvaro")){
                     Toast.makeText(MainActivity.this, "Login Sukses", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(MainActivity.this, Recyclerview.class);
-                    intent.putExtra("Username", edt_username.getText().toString());
-                    startActivity(intent);
+                    editor = pref.edit();
+                    editor.putString("username", edt_username.getText().toString());
+                    editor.putString("status", "login");
+                    editor.apply();
+                    startActivity(new Intent(getApplicationContext(), MainMenu.class));
+                    finish();
+
                 } else {
                     edt_username.setError("Invalid Username");
                     edt_password.setError("Invalid Password");
